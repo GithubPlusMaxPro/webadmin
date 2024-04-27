947349f2ad91a094e1dad77590adb325d9396a29
xt\xtli1 <xt199512@gmail.com>
2024-04-27 10:16:16 +0800

    no message

---
+++ public/assets/js/backend/incubator.js
@@ -0,0 +1,55 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'incubator/index' + location.search,
+                    add_url: 'incubator/add',
+                    edit_url: 'incubator/edit',
+                    del_url: 'incubator/del',
+                    multi_url: 'incubator/multi',
+                    import_url: 'incubator/import',
+                    table: 'incubator',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        // {field: 'id', title: __('Id')},
+                        {field: 'wdz', title: __('Wdz'), operate: 'LIKE', table: table, class: 'autocontent', formatter: Table.api.formatter.content},
+                        {field: 'sdz', title: __('Sdz'), operate: 'LIKE', table: table, class: 'autocontent', formatter: Table.api.formatter.content},
+                        {field: 'wdz1', title: __('Wdz1'), operate: 'LIKE', table: table, class: 'autocontent', formatter: Table.api.formatter.content},
+                        {field: 'sdz1', title: __('Sdz1'), operate: 'LIKE', table: table, class: 'autocontent', formatter: Table.api.formatter.content},
+                        {field: 'dt', title: __('Dt'), operate:'RANGE', addclass:'datetimerange', autocomplete:false},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
